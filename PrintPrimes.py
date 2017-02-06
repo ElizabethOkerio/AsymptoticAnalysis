2d4d5ce7ea3e064804cb8137c28fd608a30a5869
Elizabeth <elizabeth@cs4africa.com>
2017-02-07 00:18:20 +0300

    Commit Two

---
+++ PrintPrimes.py
@@ -0,0 +1,33 @@
+function PrintPrimeNumberInARange(n):
+
+for num in range(0,n):
+
+	if num>1:
+
+	for i in range (2,n):
+            
+        if i==2:
+
+			print i
+
+		elif i==3:
+
+			print i
+
+		elif i%2==0 && i>2:
+
+			break
+
+		elif i%3==0 && i>3:
+
+			break
+
+		else:
+
+			print num
+
+    
+
+
+
+
